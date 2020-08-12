b1c318b1254536c6165adf579d8ca447c646289c
Jeremy <jeremyVDLRC@gmail.com>
2020-08-12 22:43:01 +1000

    Create connection.js and insert boiler code to connect to mysql db

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+
+// Set up our connection information
+const connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
+
+// Connect to the database
+connection.connect((err) => {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection
+module.exports = connection;
