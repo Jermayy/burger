faa21f827fc1f1b077bca68e9b85f34cfa9fee8e
Jeremy <jeremyVDLRC@gmail.com>
2020-08-16 21:29:14 +1000

    added link to remote db

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,26 @@
 const mysql = require("mysql");
 
 // Set up our connection information
-const connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
 
-// Connect to the database
-connection.connect((err) => {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection - mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
 
-// Export connection
-module.exports = connection;
+    // Connect to the database
+    connection.connect((err) => {
+        if (err) {
+            console.error("error connecting: " + err.stack);
+            return;
+        }
+        console.log("connected as id " + connection.threadId);
+    });
+
+    // Export connection
+    module.exports = connection;
