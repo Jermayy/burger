5e2cd58af510bf7722c1e264e41b3b4bec696f5e
Jeremy <jeremyVDLRC@gmail.com>
2020-08-16 20:19:33 +1000

    save connection.js without password

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "Jeremyrawrr7",
+    password: "",
     database: "burgers_db"
 });
 
