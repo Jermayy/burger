99f52eb2e0114dac1edac8f62a5a03ee8b39e0b9
Jeremy <jeremyVDLRC@gmail.com>
2020-08-16 17:54:16 +1000

    change devour column name and added additional items to 'devoured' burger items

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers(
 id INT NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR(250),
-devoured BOOLEAN NULL,
+devour BOOLEAN NULL DEFAULT false,
 PRIMARY KEY(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,7 @@
-INSERT INTO  burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
-INSERT INTO  burgers (burger_name, devoured) VALUES ('Crispy Chicken Deluxe Burger', false);
-INSERT INTO  burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO  burgers (burger_name, devour) VALUES ('Cheeseburger', false);
+INSERT INTO  burgers (burger_name, devour) VALUES ('Crispy Chicken Deluxe Burger', false);
+INSERT INTO  burgers (burger_name, devour) VALUES ('Double Bacon Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devour) VALUES ('Fish-O-Fillet', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Double pounder', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Double cheese burger', true);
