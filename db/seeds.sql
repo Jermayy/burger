970720c5dfb87ddb9b6f0c27d874c99a970d3f87
Jeremy <jeremyVDLRC@gmail.com>
2020-08-16 19:41:55 +1000

    fix up insert statements

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,6 +2,6 @@ INSERT INTO  burgers (burger_name, devour) VALUES ('Cheeseburger', false);
 INSERT INTO  burgers (burger_name, devour) VALUES ('Crispy Chicken Deluxe Burger', false);
 INSERT INTO  burgers (burger_name, devour) VALUES ('Double Bacon Cheeseburger', false);
 
-INSERT INTO burgers (burger_name, devour) VALUES ('Fish-O-Fillet', true);
-INSERT INTO burgers (burger_name, devour) VALUES ('Double pounder', true);
-INSERT INTO burgers (burger_name, devour) VALUES ('Double cheese burger', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Angus Beef Burger', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('McChicken', true);
