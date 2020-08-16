70a9d24ffa9a88f6a4bf0ba153e2c433130106ba
Jeremy <jeremyVDLRC@gmail.com>
2020-08-16 17:53:32 +1000

    revamp controllers file with updated column name

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,9 +1,11 @@
 const express = require("express");
+
 const router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
 const burger = require("../models/burger.js");
 
-// insert code here
-
+// Create all our routes and set up logic within those routes where required.
 router.get("/", (req, res) => {
     burger.selectAll((data) => {
         const hbsObject = {
@@ -16,31 +18,31 @@ router.get("/", (req, res) => {
 
 router.post("/api/burgers", (req, res) => {
     burger.insertOne([
-        "burger_name", "devoured"
+        "burger_name", "devour"
     ], [
-        req.body.burger_name, req.body.devoured
+        req.body.burger_name, req.body.devour
     ], (result) => {
+        // Send back the ID of the new quote
         res.json({ id: result.insertId });
     });
 });
 
 router.put("/api/burgers/:id", (req, res) => {
     const condition = "id = " + req.params.id;
+
     console.log("condition", condition);
 
-
     burger.updateOne({
-            devoured: req.body.devoured
-        },
-        condition, (result) => {
-            if (result.changedRows === 0) {
-                return res.status(404).end();
-            } else {
-                res.status(200).end();
-
-            }
-        });
+        devour: req.body.devour
+    }, condition, (result) => {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
 });
 
-
+// Export routes for server.js to use.
 module.exports = router;
